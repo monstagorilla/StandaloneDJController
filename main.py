b4e015f2288bc2eeca3522dbf65e0f333a0e0cb8
Jonas Schröder <jschroeder.ac@gmail.com>
2018-04-09 16:23:33 +0200

    add basic player options add 3 band EQ

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
+from __future__ import division
 from pyo import *
-s = Server()
-s.setInOutDevice(7)
-s.boot()
-path1 = "/home/monstagorilla/Documents/Coding/StandaloneDJController/test.wav"
-path2 = "/home/monstagorilla/Documents/Coding/StandaloneDJController/test2.wav"
-t = SndTable(path1)
-freq = t.getRate()
-osc = Osc(table = t, freq = freq, mul = 0.4).out()
+import sys
+import os
+
+def init():
+    s = Server()
+    s.setInOutDevice(7)
+    s.boot()
+    return s
+   
+s = init()
+snd = SndTable('/home/monstagorilla/Documents/Coding/StandaloneDJController/test.wav')
+p = Phasor(freq=snd.getRate()*1.1)
+a = Pointer(table=snd, index = p, mul = 0.3)
+n = Noise()
+lowEq = EQ(input = a, boost = 1, freq = 125, q = 1, type = 1)
+midEq = EQ(input = lowEq, boost = 1, freq = 1200, q = 0.5, type = 0) 
+highEq = EQ(input = midEq, boost = 1, freq = 8000, q = 1, type =2).out()
+lowEq.ctrl(title="low")
+midEq.ctrl(title="mid")
+highEq.ctrl(title="high")
+#p.ctrl(title="pitch")
+spec = Spectrum(highEq, size=1024)
 s.gui(locals())
+
