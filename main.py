7c954e4690d31adc97f4d8832528a4d2f20178f7
Jonas Schröder <jschroeder.ac@gmail.com>
2018-04-17 23:51:55 +0200

    event handler for terminal input

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyo import *
 import wx
+
 class Player:
     def __init__(self):
         self.server = Server()
@@ -35,6 +36,7 @@ class Player:
         self.highEq[0].ctrl(title = "high0")
         self.highEq[1].ctrl(title = "high1")
 
+
     def load_track(self, path, channel):
         self.table[channel] = SndTable(path)
         self.phasor[channel].freq = self.table[channel].getRate()
@@ -48,18 +50,21 @@ class Player:
             self.phasor[channel].freq = self.table[channel].getRate() * self.pitch[channel]
             self.isPlaying[channel] = True
 
-    def setPitch(self, value, channel):
+    def set_pitch(self, value, channel):
         self.pitch[channel] = value
         self.phasor[channel].freq = value * self.table[channel].getRate()
 
-    def setPostion(self, position, channel):
-        self.phasor[channel].phase = postion
+    def set_position(self, position, channel):
+        self.phasor[channel].reset()
+        self.phasor[channel].phase = position
 
-    def jumpPosition(self, diff, channel):
-        if 0 <= self.phasor[channel].phase + diff <= 1:
+    def jump_position(self, diff, channel):
+        if 0 <= (self.phasor[channel].phase + diff) and (self.phasor[channel].phase + diff) <= 1:
             self.phasor[channel].phase += diff
+        else:
+            print("jump not in range")
 
-    def setEq(equalizer, value):
+    def set_eq(self, equalizer, value):
         if value > 0:
             value /= 10 #weaker increasing than lowering
         equalizer.boost = value * 40 #max lowering 40dB
@@ -67,14 +72,65 @@ class Player:
 p = Player()
 p.load_track('test.wav', 0)
 p.load_track('test2.wav',1)
-
 p.start_stop(0)
 p.start_stop(1)
+p.server.start()
+
+is_running = True
+
+
+
+while is_running:
+    new_input = input("Action:")
+    if new_input == "s0":
+        p.start_stop(0)
+    elif new_input == "s1":
+        p.start_stop(1)
+    elif new_input == "pitch0":
+        p.set_pitch(float(input("pitch in percent channel 0")), 0)
+    elif new_input == "pitch1":
+        p.set_pitch(float(input("pitch in percent channel 1")), 1)
+    elif new_input == "pos0":
+        p.set_position(float(input("position channel 0 between 0 and 1")), 0)
+    elif new_input == "pos1":
+        p.set_position(float(input("position channel 1 between 0 and 1")), 1)
+    elif new_input == "jump0":
+        p.jump_position(float(input("jump diff channel 0 between 0 and 1")), 0)
+    elif new_input == "jump1":
+        p.jump_position(float(input("jump diff channel 1 between 0 and 1")), 1)
+            
+    elif new_input == "eq0":
+        which_input = input("which eq?")
+        if which_input == "low":
+            p.set_eq(p.lowEq[0], float(input("low boost channel 0 between 0 and 1")))
+        elif which_input == "mid":
+            p.set_eq(p.midEq[0], float(input("mid boost channel 0 between 0 and 1")))
+        elif which_input == "high":
+            p.set_eq(p.highEq[0], float(input("high boost channel 0 between 0 and 1")))
+        else: 
+            print("invalid eq")    
+    elif new_input == "eq1":
+        which_input = input("which eq for channel 1?")
+        if which_input == "low":
+            p.set_eq(p.lowEq[1], float(input("low boost channel 1 between 0 and 1")))
+        elif which_input == "mid":
+            p.set_eq(p.midEq[1], float(input("mid boost channel 1 between 0 and 1")))
+        elif which_input == "high":
+            p.set_eq(p.highEq[1], float(input("high boost channel 1 between 0 and 1")))
+        else: 
+            print("invalid eq")
+    elif new_input == "quit":
+        is_running = False
+    else:
+        print("invalid input")
+
+
+
 #app = wx.App()
-#frame = wx.Frame(None, style= wx.NO_BORDER | wx.CAPTION)
+#frame = wx.Frame(None, style = wx.NO_BORDER | wx.CAPTION)
 #frame.Show()
 #snd_view = PyoGuiSndView(parent = frame)
-#snd_view.setTable(p.table)
+
+#snd_view.setTable(p.table[0])
 
 #app.MainLoop()
-p.server.gui()
