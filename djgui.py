47ac982f13f6c85d782f37ac41891b64f5a6c720
Jonas Schröder <jonas.schroeder@posteo.de>
2018-10-07 16:38:32 +0200

    WIP box layout with hardcoded rendering

--- djgui.py
+++ djgui.py
@@ -1,42 +1,48 @@
-from math import sin
 from scipy.io import wavfile
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.properties import StringProperty, ListProperty, NumericProperty
+from kivy.properties import StringProperty, ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.graphics import Line, Color
+from kivy.core.window import Window
 from numpy import mean
-from math import log
 
 class AudioVisualizer(Widget):
-
-
+    some_val = ObjectProperty([])
     def __init__(self, **kwargs):
         super(AudioVisualizer, self).__init__(**kwargs)
         raw_data = wavfile.read('/home/monstagorilla/Music/Indigo (Alex Niggemann Remix).wav')[1]
-        data = []
+        abs_data = []
         for x in raw_data[::50]:
-            data.append(abs(x[0]))
+            abs_data.append(abs(x[0]))
 
-        win_width = 400
-        chunk_size = int(len(data)/win_width)
-        line_points = []
-        for x in range(0, win_width):
+        width = 250
+        chunk_size = int(len(abs_data)/width)
+        mean_data = []
+        for x in range(0, width):
             try:
-                line_points.append(mean(data[x * chunk_size: (x + 1) * chunk_size]))
+                mean_data.append(mean(abs_data[x * chunk_size: (x + 1) * chunk_size]))
             except:
                 pass
 
-        line_points = [x * 40 / max(line_points) for x in line_points]
+        max_val = max(mean_data)
+        amp = 20
+        self.line_points = []
+        for x in range(0, width):
+            self.line_points.extend([300 + x, 400, 300 + x, 400 + mean_data[x] * amp / max_val])
 
         with self.canvas.after:
             Color(1, 0.5, 1)
-            for x in range(0, len(line_points)):
-                Line(points=[self.center_x + x, self.center_y - line_points[x], self.center_x + x, self.center_y + line_points[x]])
+            Line(points=self.line_points)
+
+
 
 class DJGUI(BoxLayout):
-    time = StringProperty("0:00/0:00")
-    pass
+    time = StringProperty(["0:00/0:00", "0:00/0:00"])
+    track_name = StringProperty(["Pineapple Thief - Threatening War ", "Nirvana - Come A You Are"])
+    bpm = StringProperty(["122", "79"])
+
+
 
 class DJGUIApp(App):
     def build(self):
@@ -44,5 +50,7 @@ class DJGUIApp(App):
 
 
 app = DJGUIApp()
+#Window.fullscreen = True
+
 
 app.run()
