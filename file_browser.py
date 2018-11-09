5d6fb43d44bffa11d75c4cf6596a7229e1e83430
Jonas Schröder <jonas.schroeder@posteo.de>
2018-11-09 14:14:58 +0100

    improved key support in filechooser

--- file_browser.py
+++ file_browser.py
@@ -19,7 +19,6 @@ class FileBrowser(FileChooserListView):
     root_path = StringProperty(os.path.expanduser("~/Music"))
     def __init__(self):
         super(FileChooserListView, self).__init__()
-        self.path = ""
         self.path_root = ""
         self.path_temp = os.path.expanduser("~/temp_standalone_dj_controller")
 
@@ -36,6 +35,9 @@ class FileBrowser(FileChooserListView):
         self.track_list = []
         self.index = 0
         self.dir_lvl = 0
+
+        if len(self._items) > 0:
+            self._items[0].is_selected = True
 
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
@@ -72,29 +74,42 @@ class FileBrowser(FileChooserListView):
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         if keycode[1] == 'down':
             count = len(self._items)
+            has_selected = False
             for i in range(0, count):
-                if self._items[i].is_selected and i < count - 1:
-                    self._items[i].is_selected = False
-                    self._items[i + 1].is_selected = True
+                if self._items[i].is_selected:
+                    has_selected = True
+                    if i < count - 1:
+                        self._items[i].is_selected = False
+                        self._items[i + 1].is_selected = True
                     break
+            if not has_selected:
+                self._items[0].is_selected = True
         elif keycode[1] == 'up':
             count = len(self._items)
+            has_selected = False
             for i in range(0, count):
-                if self._items[i].is_selected and i > 0:
-                    self._items[i].is_selected = False
-                    self._items[i].selected = False
-                    self._items[i - 1].is_selected = True
-                    self._items[i - 1].selected = True
-
+                if self._items[i].is_selected:
+                    has_selected = True
+                    if i > 0:
+                        self._items[i].is_selected = False
+                        self._items[i - 1].is_selected = True
                     break
+            if not has_selected:
+                self._items[count - 1].is_selected = True
+
         elif keycode[1] == 'enter':
             for x in self._items:
                 if x.is_selected:
                     if self.file_system.is_dir(x.path):
                         self.path = x.path
+                        if len(self._items) > 0:
+                            self._items[0].is_selected = True
+
                     else:
                         print("load_track")
-
+        elif keycode[1] == 'left':
+            if self.path != self.rootpath:
+                self.path = "/".join(self.path.split("/")[:-1])
 
 
 
