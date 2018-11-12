a9c9b287e788df4d6629b4460659ee48762abe4a
Jonas Schröder <jonas.schroeder@posteo.de>
2018-11-12 20:36:27 +0100

    clean code and add logging

--- player.py
+++ player.py
@@ -1,25 +1,28 @@
-from pyo import *
+# CLEANED UP
 
-import shutil
+from pyo import *
+import logging
+logging.basicConfig(level=logging.INFO, format='%(levelname)s:%(message)s')
+
 
 class Player:
-    def __init__(self, gui) -> None:
-        self.gui = gui
+    def __init__(self) -> None:
+        # Properties
         self.server = Server()
-        self.server.setInOutDevice(6)
-        #self.server.setInputDevice(6)
-        #self.server.setOutputDevice(6)
+        self.server.setInOutDevice(8)
         self.server.boot()
         self.server.start()
         self.table = [SndTable(), SndTable()]
-        self.mono_table = [SndTable(chnl=0), SndTable(chnl=0)]
         self.title = ["", ""]
         self.isPlaying = [False, False]
+        self.refresh_snd = [False, False]
+
+        # Audio Modules
         self.phasor = [Phasor(freq=0), Phasor(freq=0)]
         self.pointer = [Pointer(table=self.table[0], index=self.phasor[0], mul=0.3),
-                        Pointer(table=self.table[1], index=self.phasor[1], mul=0.3)]
+                        Pointer(table=self.table[1], index=self.phasor[1], mul=0.3)]  # TODO check # mul != 1n?
         self.pitch = [1, 1]
-        self.lowEq = [EQ(input=self.pointer[0], boost=1, freq=125, q=1, type=1),
+        self.lowEq = [EQ(input=self.pointer[0], boost=1, freq=125, q=1, type=1),  # TODO good choice for frequencies?
                       EQ(input=self.pointer[1], boost=1, freq=125, q=1, type=1)]
         self.midEq = [EQ(input=self.lowEq[0], boost=1, freq=1200, q=0.5, type=0),
                       EQ(input=self.lowEq[1], boost=1, freq=1200, q=0.5, type=0)]
@@ -28,19 +31,12 @@ class Player:
         self.mixer = Mixer(outs=1, chnls=2)
         self.mixer.addInput(0, self.highEq[0])
         self.mixer.addInput(1, self.highEq[1])
-        self.mixer.setAmp(0, 0, 1)
+        self.mixer.setAmp(0, 0, 1)  # TODO controlling volume
         self.mixer.setAmp(1, 0, 1)
         self.mainVolume = Mixer(outs=1, chnls=2)
         self.mainVolume.addInput(0, self.mixer[0])
         self.mainVolume.setAmp(0, 0, 1)
         self.mainVolume.out(1)
-        self.lowEq[0].ctrl(title="low0")
-        self.lowEq[1].ctrl(title="low1")
-        self.midEq[0].ctrl(title="mid0")
-        self.midEq[1].ctrl(title="mid1")
-        self.highEq[0].ctrl(title="high0")
-        self.highEq[1].ctrl(title="high1")
-        self.refresh_snd = [False, False]
 
     def start_stop(self, channel: int) -> None:
         if self.isPlaying[channel]:
@@ -51,12 +47,13 @@ class Player:
             self.isPlaying[channel] = True
 
     def set_pitch(self, value: int, channel: int) -> None:
-        if not self.isPlaying[channel]:
-            return
         self.pitch[channel] = value
-        self.phasor[channel].freq = value * self.table[channel].getRate()
+        if self.table[channel] is not None:
+            self.phasor[channel].freq = value * self.table[channel].getRate()
+        else:
+            logging.INFO("no track loaded")
 
-    def set_position(self, position: float, channel: int) -> None:
+    def set_position(self, position: float, channel: int) -> None:  # TODO not using this function yet
         self.phasor[channel].reset()
         self.phasor[channel].phase = position
 
@@ -64,8 +61,9 @@ class Player:
         if 0 <= self.phasor[channel].phase + diff <= 1:
             self.phasor[channel].phase += diff
         else:
-            print("jump not in range")
+            logging.INFO("tried to jump out of bounds")
 
+    # TODO universal system of input values maybe between -1 and 1
     def set_eq(self, equalizer: EQ, value: float) -> None:
         if value > 0:
             value /= 10  # weaker increasing than lowering
